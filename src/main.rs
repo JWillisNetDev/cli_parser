ae78a8f76a593dee8564ddec449ebf276a29d14b
Joshua Willis <joshua.willis@intellitect.com>
2024-11-19 00:01:23 -0800

    Write naive implementation

--- src/main.rs
+++ src/main.rs
@@ -1,163 +1,176 @@
 use access_log_parser::{parse, CombinedLogEntry, LogEntry, LogType};
 use chrono::{DateTime, FixedOffset};
-use std::{boxed::Box, cmp::Ordering, fs::File, io::BufRead, net::{IpAddr, Ipv4Addr}};
+use http::StatusCode;
+use std::{fs::File, io::BufRead, net::IpAddr, str::FromStr};
+use rs_filter::{Filterable, filter_for, EqFilter, OrdFilter, StringFilter};
+use std::path::PathBuf;
+use clap::{Args, Parser, Subcommand};
 
-type Error = Box<dyn std::error::Error + Send + Sync>;
+// desired syntax:
+// log-filter <file> filter --user-agent contains "Chrome"
+// log-filter <file> filter --ip eq "193.105.7.171"
+// log-filter <file> filter --timestamp gt "2023-02-12T14:34:20+00:00" --ip eq "193.105.7.171"
 
-#[derive(Debug)]
-enum Filter<T> {
-    None,
-    Any,
-    Eq(T),
-    Gt(T),
-    Gte(T),
-    Lt(T),
-    Lte(T),
+#[derive(Parser, Debug)]
+#[command(about = "Parse logs from a given file", name = "log-parser")]
+struct Cli {
+    file: PathBuf,
+    #[command(subcommand)]
+    command: Commands,
 }
 
-impl<T> std::default::Default for Filter<T> {
-    fn default() -> Self {
-        Self::Any
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Filter(FilterArgs)
+}
+
+#[derive(Args, Debug)]
+struct FilterArgs {
+    #[arg(short, long)]
+    status_code: Option<String>,
+    
+    #[arg(short, long)]
+    user_agent: Option<String>,
+    
+    #[arg(short, long)]
+    ip: Option<String>,
+
+    #[arg(short, long)]
+    timestamp: Option<String>,
+}
+
+fn explode_args(value: &str) -> Result<Vec<&str>, String> {
+    let args: Vec<&str> = value.splitn(2, ' ').collect();
+
+    if args.len() != 2 {
+        Err(format!("Invalid filter {}", value))
+    }
+    else {
+        Ok(args)
     }
 }
 
-trait EqFilterable<T: PartialEq> {
-    fn apply_eq_filter(&self, filter: &Filter<T>) -> bool;
+fn parse_or_err<T: FromStr>(value: &str) -> Result<T, String> {
+    value.parse().map_err(|_| format!("Invalid value for filter: {}", value))
 }
 
-impl<T: PartialEq> EqFilterable<T> for T {
-    fn apply_eq_filter(&self, filter: &Filter<T>) -> bool {
-        match filter {
-            Filter::None => false,
-            Filter::Any => true,
-            Filter::Eq(value) => self == value,
-            _ => false,
+fn parse_string_filter(value: impl AsRef<str>) -> Result<StringFilter, String> {
+    let value = value.as_ref();
+    if value == "none" {
+        Ok(StringFilter::None)
+    }
+    else {
+        let args = explode_args(value)?;
+        match args[0] {
+            "contains" => Ok(StringFilter::Contains(args[1].to_string())),
+            "eq" => Ok(StringFilter::Eq(args[1].to_string())),
+            "starts_with" => Ok(StringFilter::StartsWith(args[1].to_string())),
+            "ends_with" => Ok(StringFilter::EndsWith(args[1].to_string())),
+            _ => Err(format!("Invalid filter {}", value))
         }
     }
 }
 
-impl<T: PartialEq> EqFilterable<T> for Option<T> {
-    fn apply_eq_filter(&self, filter: &Filter<T>) -> bool {
-        match filter {
-            Filter::Any => true,
-            Filter::None => self.is_none(),
-            Filter::Eq(value) => self.as_ref() == Some(value),
-            _ => false,
+fn parse_eq_filter<T: PartialEq + FromStr>(value: impl AsRef<str>) -> Result<EqFilter<T>, String> {
+    let value = value.as_ref();
+    if value == "none" {
+        Ok(EqFilter::None)
+    }
+    else {
+        let args = explode_args(value)?;
+        match args[0] {
+            "eq" => Ok(EqFilter::Eq(parse_or_err(args[1])?)),
+            "neq" => Ok(EqFilter::Neq(parse_or_err(args[1])?)),
+            _ => Err(format!("Invalid filter {}", value))
         }
     }
 }
 
-trait OrdFilterable<T: PartialOrd> {
-    fn apply_ord_filter(&self, filter: &Filter<T>) -> bool;
-}
-
-impl<T: PartialOrd> OrdFilterable<T> for T {
-    fn apply_ord_filter(&self, filter: &Filter<T>) -> bool {
-        match filter {
-            Filter::None => false,
-            Filter::Any => true,
-            Filter::Eq(value) => self == value,
-            Filter::Gt(value) => self > value,
-            Filter::Gte(value) => self >= value,
-            Filter::Lt(value) => self < value,
-            Filter::Lte(value) => self <= value,
+fn parse_ord_filter<T: PartialOrd + FromStr>(value: impl AsRef<str>) -> Result<OrdFilter<T>, String> {
+    let value = value.as_ref();
+    if value == "none" {
+        Ok(OrdFilter::None)
+    }
+    else {
+        let args = explode_args(value)?;
+        match args[0] {
+            "eq" => Ok(OrdFilter::Eq(parse_or_err(args[1])?)),
+            "neq" => Ok(OrdFilter::Neq(parse_or_err(args[1])?)),
+            "gt" => Ok(OrdFilter::Gt(parse_or_err(args[1])?)),
+            "lt" => Ok(OrdFilter::Lt(parse_or_err(args[1])?)),
+            "gte" => Ok(OrdFilter::Gte(parse_or_err(args[1])?)),
+            "lte" => Ok(OrdFilter::Lte(parse_or_err(args[1])?)),
+            _ => Err(format!("Invalid filter {}", value))
         }
     }
 }
 
-impl<T: PartialOrd> OrdFilterable<T> for Option<T> {
-    fn apply_ord_filter(&self, filter: &Filter<T>) -> bool {
-        match filter {
-            Filter::Any => true,
-            Filter::None => self.is_none(),
-            Filter::Eq(value) => self.as_ref() == Some(value),
-            Filter::Gt(value) => self.as_ref().map_or(false, |v| v > value),
-            Filter::Gte(value) => self.as_ref().map_or(false, |v| v >= value),
-            Filter::Lt(value) => self.as_ref().map_or(false, |v| v < value),
-            Filter::Lte(value) => self.as_ref().map_or(false, |v| v <= value),
-        }
+impl TryFrom<FilterArgs> for LogFilter {
+    type Error = String;
+
+    fn try_from(value: FilterArgs) -> Result<Self, Self::Error> {
+        Ok(LogFilter {
+            status_code: value.status_code.map_or(Ok(EqFilter::Any), parse_eq_filter)?,
+            user_agent: value.user_agent.map_or(Ok(StringFilter::Any),parse_string_filter)?,
+            ip: value.ip.map_or(Ok(EqFilter::Any), parse_eq_filter)?,
+            timestamp: value.timestamp.map_or(Ok(OrdFilter::Any), parse_ord_filter)?,
+        })
     }
 }
 
-enum StringFilter<'a> {
-    None,
-    Any,
-    Eq(&'a str),
-    Contains(&'a str),
-    NotContains(&'a str),
+#[filter_for(CombinedLogEntry<'a>)]
+struct LogFilter {
+    user_agent: StringFilter,
+    status_code: EqFilter<StatusCode>,
+    ip: EqFilter<IpAddr>,
+    timestamp: OrdFilter<DateTime<FixedOffset>>,
 }
 
-impl Default for StringFilter<'_> {
-    fn default() -> Self {
-        Self::Any
-    }
-}
+fn main() -> Result<(), String> {
+    let cli = Cli::parse();
 
-trait StringFilterable {
-    fn apply_string_filter(&self, filter: &StringFilter) -> bool;
-}
+    match cli.command {
+        Commands::Filter(args) => {
+            let filter: LogFilter = args.try_into()?;
 
-impl StringFilterable for &str {
-    fn apply_string_filter(&self, filter: &StringFilter) -> bool {
-        match filter {
-            StringFilter::None => false,
-            StringFilter::Any => true,
-            StringFilter::Eq(value) => self == value,
-            StringFilter::Contains(value) => self.contains(value),
-            StringFilter::NotContains(value) => !self.contains(value),
-        }
-    }
-}
+            let file = File::open(cli.file).map_err(|e| e.to_string())?;
+            let reader = std::io::BufReader::new(file);
+            let lines = reader
+                .lines()
+                .filter(|l| l.as_ref().is_ok_and(|l| !l.is_empty()))
+                .map(|l| l.unwrap());
 
-impl StringFilterable for Option<&str> {
-    fn apply_string_filter(&self, filter: &StringFilter) -> bool {
-        match filter {
-            StringFilter::Any => true,
-            StringFilter::None => self.is_none(),
-            StringFilter::Eq(value) => self.as_ref().map_or(false, |v| &v == &value),
-            StringFilter::Contains(value) => self.as_ref().map_or(false, |v| v.contains(value)),
-            StringFilter::NotContains(value) => self.as_ref().map_or(false, |v| !v.contains(value)),
-        }
-    }
-}
-
-#[derive(Default)]
-struct LogFilter<'a> {
-    ip: Filter<IpAddr>,
-    timestamp: Filter<DateTime<FixedOffset>>,
-    user_agent: StringFilter<'a>,
-}
-
-impl PartialEq<CombinedLogEntry<'_>> for LogFilter<'_> {
-    fn eq(&self, other: &CombinedLogEntry) -> bool {
-        other.ip.apply_eq_filter(&self.ip)
-            && other.timestamp.apply_ord_filter(&self.timestamp)
-            && other.user_agent.apply_string_filter(&self.user_agent)
-    }
-}
-
-fn main() -> Result<(), Error> {
-    let filter = LogFilter {
-        ip: Filter::Any,
-        timestamp: Filter::Any, //Filter::Gt(DateTime::parse_from_rfc3339("2023-02-12T14:34:05+00:00")?),
-        user_agent: StringFilter::Contains("Android"),
-    };
-
-    let file = File::open("raw/data-1.log")?;
-    let reader = std::io::BufReader::new(file);
-    let lines = reader
-        .lines()
-        .filter(|l| l.as_ref().is_ok_and(|l| !l.is_empty()))
-        .map(|l| l.unwrap());
-    for line in lines {
-        let entry = parse(LogType::CombinedLog, &line)?;
-
-        if let LogEntry::CombinedLog(log) = entry {
-            if filter.eq(&log) {
-                println!("{line}");
+            for line in lines {
+                let entry = parse(LogType::CombinedLog, &line).map_err(|e| e.to_string())?;
+                if let LogEntry::CombinedLog(entry) = entry {
+                    if entry.is_match(&filter) {
+                        println!("{}", line);
+                    }
+                }
             }
         }
     }
+
+    // let filter = LogFilter {
+    //     user_agent: StringFilter::Any,
+    //     ip: EqFilter::Eq(IpAddr::V4(Ipv4Addr::new(193, 105, 7, 171))),
+    //     timestamp: OrdFilter::Any, // OrdFilter::Gt(DateTime::parse_from_rfc3339("2023-02-12T14:34:20+00:00")?),
+    // };
+
+    // let file = File::open("raw/data-1.log")?;
+    // let reader = std::io::BufReader::new(file);
+    // let lines = reader
+    //     .lines()
+    //     .filter(|l| l.as_ref().is_ok_and(|l| !l.is_empty()))
+    //     .map(|l| l.unwrap());
+    // for line in lines {
+    //     let entry = parse(LogType::CombinedLog, &line)?;
+    //     if let LogEntry::CombinedLog(entry) = entry {
+    //         if entry.is_match(&filter) {
+    //             println!("{}", line);
+    //         }
+    //     }
+    // }
 
     Ok(())
 }
